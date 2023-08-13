a0f86091b6f1ca6b4ce5b5f13fd6e11450d6f8ba
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2023-08-13 11:34:29 -0400

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Jo Lingenfelter on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
