f355cba1ef0aa5ead4b1582e67591ede9959b158
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2023-08-14 11:13:22 -0400

    Fetch movie posters

--- MovieList/MovieListRow.swift
+++ MovieList/MovieListRow.swift
@@ -10,7 +10,29 @@ import SwiftUI
 struct MovieListRow: View {
     let movie: Movie
 
+    @ScaledMetric(relativeTo: .headline)
+    private var height: CGFloat = 80
+
     var body: some View {
-        Text(movie.title)
+        HStack {
+            AsyncImage(url: movie.posterURL) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                } else if phase.error != nil {
+                    Image(systemName: "popcorn.circle")
+                        .background(Color.red.opacity(0.8))
+                } else {
+                    ProgressView()
+                }
+            }
+            .frame(maxWidth: 80)
+
+            Text(movie.title)
+                .font(.headline)
+                .padding()
+        }
+        .frame(height: height)
     }
 }
