bf2b8181ec7fa6e7d70a4aebc3c8e858cb9fe3a6
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2023-08-14 09:32:34 -0400

    Refine Movie model

--- MovieList/Models/Movie.swift
+++ MovieList/Models/Movie.swift
@@ -8,20 +8,42 @@
 import Foundation
 
 struct Movie: Codable, Identifiable {
-    let title: String?
+    let title: String
     let releaseDate: String?
-    let overview: String?
-    let genre: Genre?
+    let overview: String
+    let genreIds: [Int]
     let id: Int
-    let moviePosterPath: String?
+    let posterPath: String?
+    let originalLanguage: String
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case releaseDate = "release_date"
+        case overview
+        case genreIds = "genre_ids"
+        case id
+        case posterPath = "poster_path"
+        case originalLanguage = "original_language"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.releaseDate = try container.decodeIfPresent(String.self, forKey: .releaseDate)
+        self.overview = try container.decode(String.self, forKey: .overview)
+        self.genreIds = try container.decode([Int].self, forKey: .genreIds)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.posterPath = try container.decodeIfPresent(String.self, forKey: .posterPath)
+        self.originalLanguage = try container.decode(String.self, forKey: .originalLanguage)
+    }
 }
 
 extension Movie {
-    var moviePosterURL: URL? {
-        guard let moviePosterPath else {
+    var posterURL: URL? {
+        guard let posterPath else {
             return nil
         }
 
-        return URL(string: "https://image.tmdb.org/t/p/w500\(moviePosterPath)")!
+        return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")!
     }
 }
--- MovieList/MovieListRow.swift
+++ MovieList/MovieListRow.swift
@@ -11,8 +11,6 @@ struct MovieListRow: View {
     let movie: Movie
 
     var body: some View {
-        if let title = movie.title {
-            Text(title)
-        }
+        Text(movie.title)
     }
 }
