754432a3bafe1c5c5a6fcb6c0b33b08be424c65a
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2023-08-14 10:46:54 -0400

    Implement fetching while scrolling

---
+++ MovieList/MoviesListController.swift
@@ -0,0 +1,46 @@
+//
+//  MoviesListController.swift
+//  MovieList
+//
+//  Created by Jo Lingenfelter on 8/14/23.
+//
+
+import SwiftUI
+
+struct MoviesListState {
+    var movies: [Movie] = []
+    var pageIndex: Int = 1
+    var isLoading: Bool = false
+    var filters: [Filter] = []
+}
+
+final class MoviesListController {
+    private let apiClient = MovieListClient()
+
+    @Binding var state: MoviesListState
+
+    init(state: Binding<MoviesListState>) {
+        _state = state
+    }
+
+    func fetchMoviesIfNeeded() async throws {
+
+    }
+
+    func fetchMovies() async throws {
+        guard !state.isLoading  else {
+            return
+        }
+
+        state.isLoading = true
+
+        defer {
+            state.isLoading = false
+        }
+
+
+        let paginatedResult = try await apiClient.fetchMovies(filters: state.filters, page: state.pageIndex)
+        state.movies.append(contentsOf: paginatedResult.results)
+        state.pageIndex += 1
+    }
+}
--- MovieList/MoviesListView.swift
+++ MovieList/MoviesListView.swift
@@ -7,46 +7,32 @@
 
 import SwiftUI
 
-final class MoviesListController {
-    private(set) var movies: [Movie] = []
-    private var isFetchingMovies: Bool = false
-    private var currentPage = 1
-    private let apiClient = MovieListClient()
-
-    func fetchMovies(isLoading: Binding<Bool>? = nil) async throws {
-        guard !isFetchingMovies else {
-            return
-        }
-
-        isFetchingMovies = true
-        isLoading?.wrappedValue = true
-
-        defer {
-            isFetchingMovies = true
-            isLoading?.wrappedValue = true
-        }
-
-        
-
-    }
-
-    //private func fetchMovies(page: Int)
-}
-
 struct MoviesListView: View {
-    @State private var movies: [Movie] = []
+    @State private var state = MoviesListState()
+
+    private var controller: MoviesListController {
+        MoviesListController(state: $state)
+    }
 
     var body: some View {
         List {
-            ForEach(movies) { movie in
+            ForEach(state.movies) { movie in
                 MovieListRow(movie: movie)
+                    .onAppear {
+                        Task {
+                            do {
+                                try await controller.fetchMovies()
+                            } catch {
+                                print(error)
+                            }
+                        }
+                    }
             }
         }
         .onAppear {
             Task {
                 do {
-                    let movies = try await MovieListClient().fetchMovies(filters: [], page: 5)
-                    self.movies = movies.results
+                    try await controller.fetchMovies()
                 } catch {
                     print(error)
                 }
